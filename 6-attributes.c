33d3864bfcb66726f0a9f64ca8dfc7ff6b877a13
David Greenberg <david@david-laptop.(none)>
2009-01-22 21:47:01 -0500

    added attribute lesson

---
+++ 6-attributes.c
@@ -0,0 +1,70 @@
+#include <stdio.h>
+
+//Firstly, alignment attributes (not safe with all linkers;
+//add run-time checks if you use this or know that it may be ignored)
+
+//There are 4096 bytes in a page
+#define PGSIZE 4096
+
+//page-aligned 4096 byte structure (4 bytes per int)
+int foo[PGSIZE/4] __attribute__((aligned(PGSIZE)));
+
+//we can also make alignment an attribute on a type
+typedef int __attribute__((aligned(PGSIZE))) aligned_int;
+aligned_int x = 7;
+aligned_int y = 19;
+
+//we can also control how structs are packed
+struct Foo { //this is 8 bytes b/c of alignment
+	int a;
+	short b;
+};
+struct Bar { //this is 6 bytes b/c it's packed
+	int a;
+	short b;
+} __attribute__((packed));
+
+
+//inlining is like macros for speed
+//
+//if we dropped the always_inline, we wouldn't inline under certain
+//circumstances, like -O1 or -fno-inline
+//to see, disassemble and compile with and without this 
+static inline int add(int x, int y) __attribute__((always_inline));
+
+static inline int
+add(int x, int y)
+{
+	return x + y;
+}
+
+void old_func(void) __attribute__((deprecated));
+void old_func(void) {printf("this is deprecated\n");}
+
+//These enable other compiler optimizations
+
+//const functions (no global state)
+int const_func(int x) __attribute__((const));
+int const_func(int x) { return x + 2; }
+
+//pure functions (read only global state)
+static int a = 7;
+int pure_func(int x) __attribute__((pure));
+int pure_func(int x) { return x + a; }
+
+int
+main(int argc, char **argv)
+{
+	printf("foo is at %08x, size = %d bytes\n", &foo, sizeof(foo));
+	printf("x is at %08x\n", &x);
+	printf("y is at %08x\n", &y);
+
+	printf("sizeof(struct Foo) = %d\n", sizeof(struct Foo));
+	printf("sizeof(struct Bar) = %d\n", sizeof(struct Bar));
+
+	printf("add(1, 1) = %d\n", add(1, 1));
+
+	old_func();
+
+	return 0;
+}
--- Makefile
+++ Makefile
@@ -1,7 +1,5 @@
-all: 1-typedefs 2-special_types 3-functions 4-jumptable 5-macros
+all: 1-typedefs 2-special_types 3-functions 4-jumptable 5-macros 6-attributes
 
 %.o: %.c
 #	gcc -gnu99 $@.c
 
-clean:
-	rm -f *.o
